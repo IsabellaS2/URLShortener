58edf6161630ff19721fdfce7b620674e61f6ae7
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2024-03-28 14:57:51 +0000

    adding .env files

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 
 // Configuring PostgreSQL client
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "URL",
-  password: "Sg4eM?@9n",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 
 db.connect();
